60dfe28a70cc93dcdcd12bf60f3397a67cdc62cd
Ishwari <ishwarikulkarni23@gmail.com>
2023-01-31 15:50:55 +0530

    added animated counter-number bar

---
+++ index.js
@@ -0,0 +1,36 @@
+
+
+
+const workSection=document.querySelector('.section-work-data');
+const workObserver = new IntersectionObserver((entries, observer)=>{
+    const [entry]=entries;
+    console.log(entry);
+
+    if(!entry.isIntersecting) return;
+
+    const counterNum=document.querySelectorAll(".counter-numbers");
+const speed=10;
+counterNum.forEach((curElem )=> {
+    
+    const updateNumber=()=>{
+    const targetNumber=parseInt(curElem.dataset.number);
+    const initialNum=parseInt(curElem.innerText);
+    
+
+const incrementNumber=Math.trunc(targetNumber/speed);
+
+if(initialNum<targetNumber){
+    curElem.innerText=`${initialNum + incrementNumber}+`;
+    setTimeout(updateNumber, 100);
+}
+    };
+    updateNumber();
+});
+
+observer.unobserve(workSection);
+}, {
+    root:null,
+    threshold:0,
+});
+
+workObserver.observe(workSection);
